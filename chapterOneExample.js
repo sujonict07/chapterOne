8c58d5f66340acec905c8ff9a4b93728bec2f09a
sujonict07 <sujonict07@gmail.com>
2015-03-23 12:52:57 +0600

    chapter one examples

---
+++ chapterOneExample.js
@@ -0,0 +1,15 @@
+import math
+x=2;
+y=2;
+z = x+y;
+print abs(-10)
+print pow(2,3)
+print int(math.floor(32.9))
+print math.sqrt(9)
+print z;
+name = raw_input("What is your name ? ")
+print "Hello," +name+"!"
+radius = 5
+pi =3.1416
+#print the circumference of the circle:
+print 2* pi*radius
